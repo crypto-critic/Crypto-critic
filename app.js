de9d733a396484b43a32087aee6c1efbdab39dea
pinokara <hec.stw@gmail.com>
2019-06-25 15:29:44 +0700

    New router

--- app.js
+++ app.js
@@ -5,31 +5,36 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cors = require('cors');
+var passport = require("passport");
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(__dirname));
+app.use(passport.initialize());
+require("./server/user/passport")(passport);
 app.use("/api", bodyParser.json());
 let router = express.Router();
 app.use("/api", router);
+
 //ROUTER---------------------------------------------
-require('./server/routes/coin_router')(router);
-require('./server/routes/chart_router')(router);
-// require('./server/routes/exchange_router')(router)
+// require('./server/routes/coin_router')(router);
+// require('./server/routes/chart_router')(router);
+require('./server/routes/key')(router);
+require('./server/routes/user')(router)
 //HTML-----------------------------------------------
 app.get("*",(req, res, next) => {
     res.sendFile(__dirname + "/index.html");
 });
 //DATABASE UPDATE------------------------------------
 var dbString = 'mongodb://localhost:27017/myapp';
-mongoose.connect(dbString, { useNewUrlParser: true }, function(){
-    require('@scripts/insert').coin();
-    require('@scripts/insert').chart();
-    require('@scripts/coin/sync')();
-    require('@scripts/chart/chart_draw')()
-});
+// mongoose.connect(dbString, { useNewUrlParser: true }, function(){
+    // require('@scripts/insert').coin();
+    // require('@scripts/insert').chart();
+    // require('@scripts/coin/sync')();
+    // require('@scripts/chart/chart_draw')()
+// });
 //SERVER-------------------------------------------
 var server = app.listen(process.env.PORT || 3000, function () {
     var port = server.address().port;
