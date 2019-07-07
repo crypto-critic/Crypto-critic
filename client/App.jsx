0f7fd0b2d2d02af575759567a1af7343c8a7812c
pinokara <hec.stw@gmail.com>
2019-07-08 06:36:00 +0700

    Root files

---
+++ client/App.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Redirect, Route, Switch, withRouter} from 'react-router-dom';
+import {AppLayout} from './components/AppLayout'
+import { connect } from 'react-redux';
+
+import {LoginPage} from "./components/LoginPage";
+import {RegisterPage} from "./components/RegisterPage";
+import HomePage from './components/HomePage/HomePage';
+
+// import Loadable from "react-loadable";
+// const LoginPage = Loadable({
+//     loader: () =>
+//         import("./LoginPage"),
+//     loading: () => null
+// });
+// const RegisterPage = Loadable({
+//     loader: () =>
+//         import("./RegisterPage"),
+//     loading: () => null
+// });
+const UserRoute = ({ component: Component, logInState, ...rest }) => (
+    <Route {...rest} render={props => (
+        logInState === 'SUCCESS'
+            ? <Component {...props} />
+            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+    )} />
+);
+const AnynomousRoute = ({ component: Component, logInState, ...rest }) => (
+    <Route {...rest} render={props => (
+        logInState !== 'SUCCESS'
+            ? <Component {...props} />
+            : <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+    )} />
+);
+
+export class App1 extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const {user} = this.props;
+        let context = (
+            <Switch>
+                <UserRoute exact path="/" logInState={user.state} component={HomePage} />
+                <AnynomousRoute path="/login" logInState={user.state} component={LoginPage} />
+                <AnynomousRoute path="/register" logInState={user.state} component={RegisterPage} />
+                <Route exact path="/home" component={HomePage} />
+            </Switch>
+        );
+        return (
+            <div>
+                <AppLayout children={context}/>
+            </div>
+        );
+    }
+}
+const withRouterApp = withRouter(App1);
+
+function mapStateToProps(state) {
+    return {
+        user: state.authentication
+    };
+}
+
+const connectedApp = connect(mapStateToProps)(withRouterApp);
+export { connectedApp as App };
--- client/index.js
+++ client/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-
-import { store } from './services';
-import MainRoutes from './main-routers'
+import { PersistGate } from 'redux-persist/integration/react';
+import store from './services/store';
+import AppInit from './AppInit'
 
 render(
-    <Provider store={store}>
-        <MainRoutes />
+    <Provider store={store().store}>
+        <PersistGate loading={null} persistor={store().persistor}>
+        <AppInit />
+        </PersistGate>
     </Provider>,
     document.getElementById('wrapper')
 );
