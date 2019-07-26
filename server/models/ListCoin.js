3025c67f8b4284718bb3346d793d30a427100847
pinokara <hec.stw@gmail.com>
2019-07-26 21:31:36 +0700

    List coin model

--- server/models/ListCoin.js
+++ server/models/ListCoin.js
@@ -4,11 +4,10 @@ const Schema = mongoose.Schema;
 
 const ListSchema = new Schema({
     coinId: { type: String, unique: true, index: true },
-    name: { type: String},
-    active: { type: Boolean, default: true },
-    category: { type: String}
+    host: { type: String},
+    vpsIndex: { type: Number},
 });
 
-const List = mongoose.model('List', ListSchema);
+const List = mongoose.model('lists', ListSchema);
 
 module.exports = List;
