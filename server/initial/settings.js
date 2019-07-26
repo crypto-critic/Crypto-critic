166269a9d659e0e872ba6f78e27059fb65942ee2
pinokara <hec.stw@gmail.com>
2019-07-27 06:09:44 +0700

    Settings

---
+++ server/initial/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+    dbsettings: {
+        user: "sha1",
+        password: "sha1",
+        address: "localhost",
+        port: 27017
+    },
+    port : 3001,
+    freegeoip: {
+        api: 'https://extreme-ip-lookup.com/json/'
+    },
+    timeout: '5s',
+    user: {
+        email: 'PinokaraCrypto@gmail.com',
+        password: 'SecretToken@'
+    },
+    vpsIndex: 'vps01'
+};
