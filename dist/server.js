849c246f2f9efb7c3c6fc71a73fa925bf8ea84d0
pinokara <hec.stw@gmail.com>
2019-07-27 06:09:27 +0700

    var -> const ES6

--- dist/server.js
+++ dist/server.js
@@ -1,30 +1,26 @@
-var express = require("express");
-var path = require('path');
-var mongoose = require('mongoose');
-var middleware = require('../server/middleware');
-
-var app = express();
+const express = require("express");
+const path = require('path');
+const mongoose = require('mongoose');
+const middleware = require('../server/middleware');
+const app = express();
 middleware(app);
 
-
 app.use(express.static(__dirname) );
+
 let router = express.Router();
 app.use("/api", router);
 // require('../server/routes/coinRouter')(router);
 // require('../server/routes/chartRouter')(router);
 require('../server/routes/userRouter')(router);
 
-
 app.get("*",(req, res, next) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-var dbString = 'mongodb://localhost:27017/myapp';
+const dbString = 'mongodb://localhost:27017/myapp';
 mongoose.connect(dbString, { useNewUrlParser: true });
 
-
-var server = app.listen(process.env.PORT || 5000, function () {
-    var port = server.address().port;
-
+const server = app.listen(process.env.PORT || 5000, function () {
+    const port = server.address().port;
     console.log('Listening at http://localhost:%s', port);
 });
