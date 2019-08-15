918aca07ede126ba04a26b9cc211c27199d5659e
Long Ho <longho@dropbox.com>
2019-08-15 15:23:44 -0400

    fix

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -37,7 +37,7 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 include: [
                     client,
-                    path.join(__dirname, "node_modules/react-intl"),
+                    path.join(__dirname, "../node_modules/react-intl"),
                     // path.join(__dirname, "node_modules/intl-messageformat"),
                     // path.join(__dirname, "node_modules/intl-messageformat-parser")
                 ],
